c9a1b4736f2a37aadd7ba0b3ede5cbef3307236f
JulesSucrot <51828364+JulesSucrot@users.noreply.github.com>
2023-06-01 17:54:23 +0200

    css playlist multi

--- SpotyHub/src/script_playlist_creator_multi.js
+++ SpotyHub/src/script_playlist_creator_multi.js
@@ -17,6 +17,42 @@ const playlistName = document.getElementById("name-input");
 const numberOfTracks = document.getElementById("number-input");
 
 
+const profile = await fetchProfiles(accessToken);
+populateUIs(profile);
+
+
+
+//Functions
+
+// La fonction fetchProfile envoie une requête GET à Spotify pour obtenir le profil de l'utilisateur.
+async function fetchProfiles(token) {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    // Renvoyer le profil de l'utilisateur sous forme de JSON.
+    return await result.json();
+}
+
+
+// La fonction populateUI met à jour l'interface utilisateur avec les informations du profil de l'utilisateur.
+function populateUIs(profile) {
+    // Mettre à jour les éléments HTML avec les informations du profil de l'utilisateur.
+    // Les informations affichées incluent le nom d'affichage de l'utilisateur, l'image de profil, l'ID de l'utilisateur, le pays, l'e-mail, l'URI Spotify de l'utilisateur et l'URL de l'utilisateur.
+    document.getElementById("displayName").innerText = profile.display_name;
+    localStorage.setItem('username', profile.display_name);
+    localStorage.setItem('email', profile.email);
+    if (profile.images[0]) {
+        const profileImage = new Image(200, 200);
+        profileImage.src = profile.images[0].url;
+        profileImage.height = "40";
+        profileImage.width = "40";
+
+        document.getElementById("avatar").appendChild(profileImage);
+    }
+
+}
+
 //initialize the list of tracks
 let tracksList = [];
 
